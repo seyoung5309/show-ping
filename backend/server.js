51d2a64dc873bd95a9fd13665f9ddeb6f0d11c93
오세영 <leenight5309@naver.com>
2026-05-31 22:44:41 +0900

    fix: server.js 파일 수정

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,13 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  }),
+);
 app.use(express.json());
 
 const authRoutes = require("./routes/authRoutes");
@@ -10,10 +16,3 @@ app.use("/api/auth", authRoutes);
 app.listen(3000, () => {
   console.log("서버 실행 중: http://localhost:3000");
 });
-
-const cors = require("cors");
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-  }),
-);
