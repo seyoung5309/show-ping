110a0c8bf9639b1b10bb80805eba612ee786238f
오세영 <leenight5309@naver.com>
2026-05-31 21:42:38 +0900

    #1 feat: 로그인 기능

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const { checkNickname, signup } = require("../controllers/authController");
+
+const {
+  checkNickname,
+  signup,
+  login,
+} = require("../controllers/authController");
 
 router.post("/check-nickname", checkNickname);
 router.post("/signup", signup);
+router.post("/login", login);
 
 module.exports = router;
