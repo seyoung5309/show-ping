0dfc2344dd2584cbae70ee70080a636f7cce12f8
오세영 <leenight5309@naver.com>
2026-05-31 23:52:56 +0900

    #1 feat: 로그인 페이지 (완성)

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { login } from "../api/auth";
+import styles from "./Login.module.css";
+import logo from '../assets/logo.png';
 
 function Login() {
   const [email, setEmail] = useState("");
@@ -19,13 +21,13 @@ function Login() {
   };
 
   return (
-    <div>
-      <h1>Show Ping!</h1>
-      <h2>로그인</h2>
-
-      <div>
-        <label>이메일</label>
+    <div className={styles.main_body}>
+      <img className={styles.main_img} src={logo} alt="Show Ping! 로고"></img>
+      
+      <div className={styles.box_config}>
+        <label className={styles.main_text}>이메일</label>
         <input
+          className={styles.main_box}
           type="email"
           placeholder="이메일을 입력하세요"
           value={email}
@@ -33,9 +35,10 @@ function Login() {
         />
       </div>
 
-      <div>
-        <label>비밀번호</label>
+      <div className={styles.box_config}>
+        <label className={styles.main_text}>비밀번호</label>
         <input
+          className={styles.main_box}
           type="password"
           placeholder="비밀번호를 입력하세요"
           value={password}
@@ -43,10 +46,22 @@ function Login() {
         />
       </div>
 
-      {error && <p>{error}</p>}
+        {error && (
+        <div className={styles.overlay}>
+            <div className={styles.modal_box}>
+            <p>{error}</p>
+            <button className={styles.modal_button} onClick={() => setError("")}>확인</button>
+            </div>
+        </div>
+        )}
 
-      <button onClick={handleLogin}>로그인</button>
-      <button onClick={() => navigate("/signup")}>회원가입</button>
+      <div>
+        <button className={styles.main_box_login} onClick={handleLogin}>로그인</button>
+        <div className={styles.main_box_text}>
+            <label className={styles.main_box_text_notuser}>아직 회원이 아니라면?</label>
+            <button className={styles.main_box_text_signup} onClick={() => navigate("/signup")}>회원가입</button>
+        </div>
+      </div>
     </div>
   );
 }
