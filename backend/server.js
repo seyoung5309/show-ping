f52745c7f50546bfdfd73055a2995766cbc6707d
오세영 <leenight5309@naver.com>
2026-06-11 10:56:18 +0900

    chore: 배포 설정

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://show-ping.vercel.app",
+    credentials: true,
   }),
 );
 app.use(express.json());
