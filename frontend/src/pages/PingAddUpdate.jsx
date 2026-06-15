52643b299a704253bbf9c390ce70c673a7b50cf8
오세영 <leenight5309@naver.com>
2026-06-15 14:05:30 +0900

    chore: 오류 메시지

--- frontend/src/pages/PingAddUpdate.jsx
+++ frontend/src/pages/PingAddUpdate.jsx
@@ -117,7 +117,7 @@ function PingAddUpdate() {
 
       navigate(-1);
     } catch (err) {
-      console.error(err);
+      console.error(error.message) 
       setModal("오류가 발생했습니다. 다시 시도해주세요.");
     }
   };
